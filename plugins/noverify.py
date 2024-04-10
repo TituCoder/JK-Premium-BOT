5586e3d531afcf135fd6f55e444750764884a0b4
Mr Jaynath <141899550+sunil143786@users.noreply.github.com>
2024-04-10 23:47:57 +0530

    Update noverify.py

--- plugins/noverify.py
+++ plugins/noverify.py
@@ -3,7 +3,7 @@ from utils import *
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
 import asyncio
-
+from Biisal import *
 user_states = {}
 
 @Client.on_message(filters.group & filters.command("reset_grp"))
