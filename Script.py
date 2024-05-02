d7f13ca880ac90f8a1cda56b13e6b4ed8eff504d
Mr Jaynath <141899550+sunil143786@users.noreply.github.com>
2024-05-02 18:36:59 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -437,13 +437,14 @@ Nᴀᴍᴇ - {}"""
 </b>"""
 
     IMDB_TEMPLATE_TXT = """
-<b>
+<b>Query: {query}
+IMDb Data:
+
 🏷 Title: <a href={url}>{title}</a>
 🎭 Genres: {genres}
 📆 Year: <a href={url}/releaseinfo>{year}</a>
-🌟 Rating: <a href={url}/ratings>{rating} / 10</
-a>
-📀 RunTime: {runtime} Minutes</b> """
+⏱️ Result Shown in: {remaining_seconds} <i>seconds</i> 🔥
+🌟 Rating: <a href={url}/ratings>{rating}</a> / 10</b>"""
     
     ALL_FILTERS = """
 <b>Hᴇʏ {}, Tʜᴇsᴇ ᴀʀᴇ ᴍʏ ᴛʜʀᴇᴇ ᴛʏᴘᴇs ᴏғ ғɪʟᴛᴇʀs.</b>"""
