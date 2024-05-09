78068a2c154e9ec4cc76b66889a45d477a0150d3
Mr Jaynath <141899550+sunil143786@users.noreply.github.com>
2024-05-09 14:36:09 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,8 +37,8 @@ USERS_COUNT = int(environ.get('USERS_COUNT', "2")) # Set Referel User Count
 INVITED_USER_TRAIL = int(environ.get('INVITED_USER_TRAIL', "86400")) #set in seconds, free trail invites users in 1 day, 
 
 #streming link shortner
-STREAM_SITE = environ.get('IMPORT_JK_SITE', 'jaynath.aslink.in')
-STREAM_API = environ.get('IMPORT_JK_API', 'fbbd56e9ba6e1d89b17f38d63a6a735a93f3809b')
+STREAM_SITE = environ.get('IMPORT_JK_SITE', 'krishnalink.com')
+STREAM_API = environ.get('IMPORT_JK_API', 'ee80d092c6eae94d3e6fffc4ae60e190612b813d')
 JK_STREAM_MODE = is_enabled((environ.get('JK_STREAM_MODE', 'False')), False)
 
 #premium Users Satuts
@@ -76,9 +76,9 @@ TUTORIAL = environ.get('TUTORIAL', 'https://t.me/shortnfly_official')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 
 # files Shortner site
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'jaynath.aslink.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'fbbd56e9ba6e1d89b17f38d63a6a735a93f3809b')
-IS_SHORTLINK = is_enabled((environ.get('IS_SHORTLINK', 'False')), False)
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'krishnalink.com')
+SHORTLINK_API = environ.get('SHORTLINK_API', 'ee80d092c6eae94d3e6fffc4ae60e190612b813d')
+IS_SHORTLINK = is_enabled((environ.get('IS_SHORTLINK', 'True')), False)
 
 
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
