af91582dd7bfcec8f45fa01406bc67f5203f5f7e
Mr Jaynath <141899550+sunil143786@users.noreply.github.com>
2024-05-15 10:22:37 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -85,7 +85,7 @@ async def leave_a_chat(bot, message):
         chat = chat
     try:
         buttons = [[
-            InlineKeyboardButton('Support', url=f'https://t.me/{SUPPORT_CHAT}')
+            InlineKeyboardButton('Support', url=f'https://t.me/Jaynath_Req_Movie}')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await bot.send_message(
