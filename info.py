1ed295dfe130449086714afafc3af02d4a7bd13e
Mr Jaynath <141899550+sunil143786@users.noreply.github.com>
2024-06-09 14:30:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,9 +37,9 @@ USERS_COUNT = int(environ.get('USERS_COUNT', "10")) # Set Referel User Count
 INVITED_USER_TRAIL = int(environ.get('INVITED_USER_TRAIL', "86400")) #set in seconds, free trail invites users in 1 day, 
 
 #streming link shortner
-STREAM_SITE = environ.get('IMPORT_JK_SITE', 'aslink.in')
-STREAM_API = environ.get('IMPORT_JK_API', 'f7f5fd014daf3f5284a211b5160913a1a91b23c2')
-JK_STREAM_MODE = is_enabled((environ.get('JK_STREAM_MODE', 'False')), False)
+STREAM_SITE = environ.get('IMPORT_JK_SITE', 'livekhabar.online')
+STREAM_API = environ.get('IMPORT_JK_API', '0874049b698a3a3f446b02273602892616e244d5')
+JK_STREAM_MODE = is_enabled((environ.get('JK_STREAM_MODE', 'True')), False)
 
 #premium Users Satuts
 premium = environ.get('PREMIUM_LOGS', '-1002073485610')
@@ -67,18 +67,18 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Jaynath")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # verify Shortener 
-IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'False')), False)
+IS_VERIFY = is_enabled((environ.get('IS_VERIFY', 'True')), False)
 HOW_TO_VERIFY = environ.get('HOW_TO_VERIFY', "https://t.me/Jaynath_Backup_Channel/35")
-VERIFY2_URL = environ.get('VERIFY2_URL', "aslink.in")
-VERIFY2_API = environ.get('VERIFY2_API', "f7f5fd014daf3f5284a211b5160913a1a91b23c2")
+VERIFY2_URL = environ.get('VERIFY2_URL', "livekhabar.online")
+VERIFY2_API = environ.get('VERIFY2_API', "0874049b698a3a3f446b02273602892616e244d5")
 # how to open link
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/shortnfly_official')
 IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 
 # files Shortner site
-SHORTLINK_URL = environ.get('SHORTLINK_URL', 'aslink.in')
-SHORTLINK_API = environ.get('SHORTLINK_API', 'f7f5fd014daf3f5284a211b5160913a1a91b23c2')
-IS_SHORTLINK = is_enabled((environ.get('IS_SHORTLINK', 'False')), False)
+SHORTLINK_URL = environ.get('SHORTLINK_URL', 'livekhabar.online')
+SHORTLINK_API = environ.get('SHORTLINK_API', '0874049b698a3a3f446b02273602892616e244d5')
+IS_SHORTLINK = is_enabled((environ.get('IS_SHORTLINK', 'True')), False)
 
 
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '0').split()]
