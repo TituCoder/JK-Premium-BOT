984a50a3f9e99e16128758d443fd04a4cf7d81e8
Safari Developer <142569922+SafariDeveloper@users.noreply.github.com>
2024-07-24 05:51:46 +0530

    Fix

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -189,13 +189,12 @@ async def give_filter(client, message):
 @Client.on_message(filters.private & filters.text & filters.incoming)
 async def pm_text(bot, message):
     if not PM_FILTER and not await db.has_premium_access(message.from_user.id):
-        await auto_filter(client, message)
         content = message.text
         user = message.from_user.first_name
         user_id = message.from_user.id
         if content.startswith("/") or content.startswith("#"): return  # ignore commands and hashtags
         if user_id in ADMINS: return # ignore admins
-        await message.reply_text("<b>ए दोस्त यहां मूवी नही मिलेगा ग्रुप में मांगो 👉,@Jaynath_Req_Movie</b>")
+        await message.reply_text("<b>ए दोस्त यहां मूवी नही मिलेगा ग्रुप में मांगो 👉, @Jaynath_Req_Movie</b>")
         await bot.send_message(
             chat_id=LOG_CHANNEL,
             text=f"<b>#𝐏𝐌_𝐌𝐒𝐆\n\nNᴀᴍᴇ : {user}\n\nID : {user_id}\n\nMᴇssᴀɢᴇ : {content}</b>"
