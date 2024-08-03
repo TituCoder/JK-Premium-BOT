939560a3841d3401167270731ba7626c47c6012d
Mr Jaynath <141899550+sunil143786@users.noreply.github.com>
2024-08-03 23:45:06 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,7 +1,8 @@
 class script(object):
     START_TXT = """<b><i>ʜᴇʏ 😎</i></b> {}, {}
 <b><i> ☞ᴍʏ ɴᴀᴍᴇ ɪꜱ</i></b> <a href=https://t.me/{}>{}</a>
-<b><blockquote> ɪ ᴀᴍ ᴀ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏꜰɪʟᴛᴇʀ ʙᴏᴛ ᴡɪᴛʜ ᴍᴏʀᴇ ᴀᴅᴠᴀɴᴄᴇ ғᴇᴀᴛᴜʀᴇꜱ.!!😍 </blockquote></b>
+<b><blockquote> ɪ ᴀᴍ ᴀ ᴘᴏᴡᴇʀꜰᴜʟ ᴀᴜᴛᴏꜰɪʟᴛᴇʀ ʙᴏᴛ ᴡɪᴛʜ ᴍᴏʀᴇ ᴀᴅᴠᴀɴᴄᴇ ғᴇᴀᴛᴜʀᴇꜱ.!!😍 </blockquote></b> 
+<b><blockquote>sᴇɴᴅ ᴍᴇ ᴘʀᴇᴍɪᴜᴍ ᴜꜱᴇʀ ᴀɴʏ ᴍᴏᴠɪᴇ ᴏʀ sᴇʀɪᴇs ɴᴀᴍᴇ ᴀɴᴅ sᴇᴇ ᴍʏ ᴍᴀɢɪᴄ..✨</blockquote>  <blockquote>ғᴏʀ ᴍᴏʀᴇ ᴅᴇᴛᴀɪʟs ᴜsᴇ ᴛʜᴇ ʙᴜᴛᴛᴏɴs ʙᴇʟᴏᴡ 🤞🏻</blockquote>
 
 <spoiler> 🌿 ᴍᴀɪɴᴛᴀɪɴᴇᴅ ʙʏ :  <a href=https://t.me/Owner0423_Bot> ᴍʀ ᴊᴀʏɴᴀᴛʜ </spoiler></a>"""
 
