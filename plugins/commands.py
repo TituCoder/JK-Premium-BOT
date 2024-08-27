b1198e8046d766fc7fa081174163e5f0f9ed9138
Jk Devloper <141899550+sunil143786@users.noreply.github.com>
2024-08-27 13:52:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -151,7 +151,7 @@ async def start(client, message):
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, gtxt, temp.U_NAME, temp.B_NAME),
             reply_markup=reply_markup,
-            parse_mode=enums.ParseMode.HTML
+            has_spoiler=True, parse_mode=enums.ParseMode.HTML
         )
         return
      
